9dd51362fd52da9d98c0b430cc9bc8c82bfc960a
Darren Hizon <darren.hizon@nuworks.ph>
2020-08-18 01:38:04 +0800

    Done with changes

--- src/components/products/banner.js
+++ src/components/products/banner.js
@@ -1,20 +1,18 @@
 import { Row, Col, Image } from 'react-bootstrap';
 
-const bannerImg = 'https://deliver888.com/Content/aimage/slider/banner-3.jpg';
+const bannerImg = '/images/products_banner.png';
 
 const Banner = () => (
-    <>
-        <Row>
-            <Col className='byob-title mb-3 text-primary text-uppercase'>
-                Products
-            </Col>
-        </Row>
-        <Row>
-            <Col>
-                <Image src={bannerImg} fluid></Image>
-            </Col>
-        </Row>
-    </>
+  <>
+    <Row>
+      <Col className='byob-title mb-3 text-primary text-uppercase'>Products</Col>
+    </Row>
+    <Row>
+      <Col>
+        <Image src={bannerImg} fluid></Image>
+      </Col>
+    </Row>
+  </>
 );
 
 export default Banner;
