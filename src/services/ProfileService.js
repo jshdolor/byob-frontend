d7e5a8fdcd0b7dac6ca4d8a943fc5fddd4c8fba5
Joshua Dolor <dolorjsh@gmail.com>
2020-08-22 03:31:21 +0800

    update password

--- src/services/ProfileService.js
+++ src/services/ProfileService.js
@@ -26,6 +26,24 @@ export default class ProfileService {
             });
     }
 
+    static updatePassword({ password, password_confirmation }) {
+        const config = {
+            headers: {
+                'password-reset-token': CookieManager.get(
+                    'password-reset-token'
+                ),
+            },
+        };
+        return Client.setUrl(this.endpoint + '/password')
+            .put({ password, password_confirmation }, config)
+            .then(({ data }) => {
+                return new ProfileModel(data);
+            })
+            .catch((e) => {
+                throw new ExceptionHandler('ProfileService - updateProfile', e);
+            });
+    }
+
     static updateProfile(request) {
         return Client.setUrl(this.endpoint)
             .put(request.toJSON())
