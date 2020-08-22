8cbc1bfe964f1bf9d004b95f27ea829d659ea02f
Joshua Dolor <dolorjsh@gmail.com>
2020-08-23 01:18:21 +0800

    update cart on refillable products

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -3,17 +3,17 @@ import Head from 'next/head';
 import BlogsTPL from '../../src/layouts/Blogs/BlogsTPL';
 
 class BlogsPage extends Component {
-  state = {};
-  render() {
-    return (
-      <>
-        <Head>
-          <title>BYOB | Blogs</title>
-        </Head>
-        <BlogsTPL></BlogsTPL>
-      </>
-    );
-  }
+    state = {};
+    render() {
+        return (
+            <>
+                <Head>
+                    <title>BYOB | Blogs</title>
+                </Head>
+                <BlogsTPL></BlogsTPL>
+            </>
+        );
+    }
 }
 
 export default BlogsPage;
