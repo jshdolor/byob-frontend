8938bfe5158cd2d72623c5e7bef03f4bbdfdceee
Joshua Dolor <dolorjsh@gmail.com>
2020-08-22 15:39:15 +0800

    login on checkout

--- src/lib/postLogin.js
+++ src/lib/postLogin.js
@@ -1,12 +1,14 @@
 import CartService from '~/services/Cart/CartService';
-
+import { handle as removeItem } from '~/components/cart/RemoveCartItemButton';
+import { addToCart as addItem } from '~/components/buttons/addCart';
 import { SET_CART_ITEMS, RESET_CART } from '~/store/cart/actions';
 
 const postLogin = async (useLocalCart = false) => {
     try {
+        const browserCart = [...window.Store.getState().cart];
         const userCart = await CartService.getCart();
 
-        const forLocal = userCart.map((item) => {
+        let forLocal = userCart.map((item) => {
             return {
                 product_id: item.product_id,
                 qty: item.qty,
@@ -23,6 +25,29 @@ const postLogin = async (useLocalCart = false) => {
             type: SET_CART_ITEMS,
             payload: forLocal,
         });
+
+        if (useLocalCart) {
+            const removePromise = userCart.map(
+                async (cart) => await removeItem(cart.product_id)
+            );
+
+            await Promise.all(removePromise);
+
+            window.Store.dispatch({
+                type: RESET_CART,
+                payload: [],
+            });
+
+            const addingPromise = browserCart.map(async (cartItem) => {
+                const qty = cartItem?.qty;
+                const type = cartItem.type;
+                const product_id = cartItem?.product_id;
+
+                return await addItem(product_id, type, qty);
+            });
+
+            await Promise.all(addingPromise);
+        }
     } catch (e) {
         console.log(e);
         throw e;
