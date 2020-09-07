ef1efdb924170c3d831f8bb556aaaae1b17080fc
Darren Hizon <darren.hizon@nuworks.ph>
2020-09-07 17:21:33 +0800

    Added pagination:

--- src/components/forms/ReviewForm/ReviewForm.js
+++ src/components/forms/ReviewForm/ReviewForm.js
@@ -7,137 +7,104 @@ import ProductService from '~/services/Product';
 import ProfileService from '~/services/ProfileService';
 
 class ReviewForm extends Component {
-    state = {
+  state = {
+    rating: 0,
+    isSubmitting: false,
+    apiMessage: false,
+    visible: false,
+    name: '',
+  };
+
+  componentDidMount() {
+    ProfileService.get().then((data) => {
+      this.setState({ name: data.name });
+    });
+  }
+
+  onFinish = async (values) => {
+    const request = new PostReviewRequest({
+      ratings: this.state.rating,
+      message: values.review,
+      name: this.state.name,
+    });
+
+    this.setState({ isSubmitting: true });
+    try {
+      const apiMessage = await ProductService.postReview(this.props.product.id, request);
+      this.setState({
         rating: 0,
-        isSubmitting: false,
+        apiMessage,
+        visible: true,
+      });
+    } catch (e) {
+      this.setState({
         apiMessage: false,
-        visible: false,
-        name: '',
-    };
-
-    componentDidMount() {
-        ProfileService.get().then((data) => {
-            this.setState({ name: data.name });
-        });
+        visible: true,
+      });
     }
+    this.setState({ isSubmitting: false });
+  };
 
-    onFinish = async (values) => {
-        const request = new PostReviewRequest({
-            ratings: this.state.rating,
-            message: values.review,
-            name: this.state.name,
-        });
+  onRatingChange = (nextValue, prevValue, name) => {
+    this.setState({ rating: nextValue });
+  };
 
-        this.setState({ isSubmitting: true });
-        try {
-            const apiMessage = await ProductService.postReview(
-                this.props.product.id,
-                request
-            );
-            this.setState({
-                rating: 0,
-                apiMessage,
-                visible: true,
-            });
-        } catch (e) {
-            this.setState({
-                apiMessage: false,
-                visible: true,
-            });
-        }
-        this.setState({ isSubmitting: false });
-    };
+  handleOk = () => {
+    this.setState({ visible: false });
+    this.props.closeForm();
+  };
 
-    onRatingChange = (nextValue, prevValue, name) => {
-        this.setState({ rating: nextValue });
-    };
+  onNameChange = (e) => {
+    this.setState({ name: e.target.value });
+  };
 
-    handleOk = () => {
-        this.setState({ visible: false });
-        this.props.closeForm();
-    };
+  render() {
+    const { rating } = this.state;
+    return (
+      <>
+        {this.state.apiMessage ? (
+          <Modal visible={this.state.visible} onOk={this.handleOk} onCancel={this.handleCancel} className='byob-popup' closable={false} footer={null}>
+            <h1 className='title'>{this.state.apiMessage ? 'success' : 'error'}</h1>
+            <p>{this.state.apiMessage ?? 'Please try again'}</p>
 
-    onNameChange = (e) => {
-        this.setState({ name: e.target.value });
-    };
+            <Button type='primary' onClick={this.handleOk}>
+              Okay
+            </Button>
+          </Modal>
+        ) : (
+          ''
+        )}
 
-    render() {
-        const { rating } = this.state;
-        return (
-            <>
-                {this.state.apiMessage ? (
-                    <Modal
-                        visible={this.state.visible}
-                        onOk={this.handleOk}
-                        onCancel={this.handleCancel}
-                        className='byob-popup'
-                        closable={false}
-                        footer={null}
-                    >
-                        <h1 className='title'>
-                            {this.state.apiMessage ? 'success' : 'error'}
-                        </h1>
-                        <p>{this.state.apiMessage ?? 'Please try again'}</p>
+        <Form onFinish={this.onFinish} className='review-form'>
+          <Spin spinning={this.state.isSubmitting}>
+            <StarRatingComponent name='rate1' starCount={5} value={rating} onStarClick={this.onRatingChange} />
 
-                        <Button type='primary' onClick={this.handleOk}>
-                            Okay
-                        </Button>
-                    </Modal>
-                ) : (
-                    ''
-                )}
+            <Form.Item name='name'>
+              <Input name='name' onChange={this.onNameChange} value={this.state.name} placeholder='Name' />
+              <small name=''>If this field is blank, your review will be registered as Anonymous</small>
+            </Form.Item>
 
-                <Form onFinish={this.onFinish} className='review-form'>
-                    <Spin spinning={this.state.isSubmitting}>
-                        <StarRatingComponent
-                            name='rate1'
-                            starCount={5}
-                            value={rating}
-                            onStarClick={this.onRatingChange}
-                        />
-
-                        <Form.Item name='name'>
-                            <Input
-                                name='name'
-                                onChange={this.onNameChange}
-                                value={this.state.name}
-                                placeholder='Name'
-                            />
-                            <small name=''>
-                                If this field is blank, your review will be
-                                registered as Anonymous
-                            </small>
-                        </Form.Item>
-
-                        <Form.Item
-                            name='review'
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your review!',
-                                },
-                            ]}
-                        >
-                            <TextArea
-                                name='review'
-                                placeholder='Write a review'
-                                autoSize={{ minRows: 10, maxRows: 14 }}
-                            />
-                        </Form.Item>
-                        <Form.Item>
-                            <Button
-                                type='primary'
-                                htmlType='submit'
-                                className='submit-btn'
-                            >
-                                SEND
-                            </Button>
-                        </Form.Item>
-                    </Spin>
-                </Form>
-            </>
-        );
-    }
+            <Form.Item
+              name='review'
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your review!',
+                },
+              ]}
+            >
+              <TextArea name='review' placeholder='Write a review' autoSize={{ minRows: 10, maxRows: 14 }} maxLength={400} />
+            </Form.Item>
+            <Form.Item>
+              <Button type='primary' htmlType='submit' className='submit-btn'>
+                SEND
+              </Button>
+            </Form.Item>
+          </Spin>
+        </Form>
+      </>
+    );
+  }
 }
 
 export default ReviewForm;
--- src/layouts/Products/SingleProductTPL.jsx
+++ src/layouts/Products/SingleProductTPL.jsx
@@ -7,184 +7,148 @@ import ReviewForm from '../../components/forms/ReviewForm/ReviewForm';
 import ProductModel from '~/models/product';
 import AddCart from '~/components/buttons/addCart';
 import BuyNow from '~/components/buttons/buyNow';
+import { paginate } from '../../utils/paginate';
+import PageSelect from '../../components/PageSelect/PageSelect';
 const { Option } = Select;
-import {
-    amountPrecision,
-    refillableOptions,
-    defaultRefillableValue,
-} from '~/config/app';
+import { amountPrecision, refillableOptions, defaultRefillableValue } from '~/config/app';
 
 class SingleProductTPL extends Component {
-    state = {
-        total:
-            this.props.product.price *
-            (this.props.product.type.id === 2 ? defaultRefillableValue : 1),
-        quantity: this.props.product.type.id === 2 ? defaultRefillableValue : 1,
+  state = {
+    total: this.props.product.price * (this.props.product.type.id === 2 ? defaultRefillableValue : 1),
+    quantity: this.props.product.type.id === 2 ? defaultRefillableValue : 1,
 
-        price: this.props.product.price,
-        review: false,
-    };
+    price: this.props.product.price,
+    review: false,
+    currentPage: 1,
+    pageSize: 3,
+  };
 
-    onAdd = () => {
-        this.setState(
-            {
-                quantity: this.state.quantity + 1,
-            },
-            this.computeSubTotal
-        );
-    };
+  onAdd = () => {
+    this.setState(
+      {
+        quantity: this.state.quantity + 1,
+      },
+      this.computeSubTotal
+    );
+  };
 
-    onMinus = () => {
-        this.setState(
-            {
-                quantity:
-                    this.state.quantity === 1
-                        ? this.state.quantity
-                        : this.state.quantity - 1,
-            },
-            this.computeSubTotal
-        );
-    };
+  onMinus = () => {
+    this.setState(
+      {
+        quantity: this.state.quantity === 1 ? this.state.quantity : this.state.quantity - 1,
+      },
+      this.computeSubTotal
+    );
+  };
 
-    onQuantityChange = (value) => {
-        value = parseInt(value) ?? defaultRefillableValue;
+  onQuantityChange = (value) => {
+    value = parseInt(value) ?? defaultRefillableValue;
 
-        this.setState(
-            {
-                quantity: this.state.quantity > 0 ? value : 1,
-            },
-            this.computeSubTotal
-        );
-    };
+    this.setState(
+      {
+        quantity: this.state.quantity > 0 ? value : 1,
+      },
+      this.computeSubTotal
+    );
+  };
 
-    computeSubTotal = () => {
-        const subtotal = this.state.quantity * this.state.price;
+  computeSubTotal = () => {
+    const subtotal = this.state.quantity * this.state.price;
 
-        this.setState({
-            total: parseFloat(subtotal).toFixed(amountPrecision),
-        });
-    };
+    this.setState({
+      total: parseFloat(subtotal).toFixed(amountPrecision),
+    });
+  };
 
-    showReview = () => {
-        this.setState({
-            review: !this.state.review,
-        });
-    };
+  showReview = () => {
+    this.setState({
+      review: !this.state.review,
+    });
+  };
 
-    componentDidUpdate(prevProps) {
-        //resolves clicking a suggested product within the page - values stays the same without this
-        if (prevProps.product.id !== this.props.product.id) {
-            const { type, price } = new ProductModel(this.props.product);
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
 
-            this.setState({
-                price,
-                total: price * (type.id === 2 ? defaultRefillableValue : 1),
-                quantity: type.id === 2 ? defaultRefillableValue : 1,
-            });
-        }
+  componentDidUpdate(prevProps) {
+    //resolves clicking a suggested product within the page - values stays the same without this
+    if (prevProps.product.id !== this.props.product.id) {
+      const { type, price } = new ProductModel(this.props.product);
+
+      this.setState({
+        price,
+        total: price * (type.id === 2 ? defaultRefillableValue : 1),
+        quantity: type.id === 2 ? defaultRefillableValue : 1,
+      });
     }
+  }
 
-    render() {
-        const { quantity, total } = this.state;
-        const {
-            type,
-            description,
-            displayPrice,
-            name,
-            brand,
-            id,
-            image,
-            average_ratings,
-        } = new ProductModel(this.props.product);
+  render() {
+    const { quantity, total, currentPage, pageSize } = this.state;
+    const { type, description, displayPrice, name, brand, id, image, average_ratings } = new ProductModel(this.props.product);
 
-        const suggestions = this.props.suggestions.map(
-            (product) => new ProductModel(product)
-        );
-        const { reviews } = this.props;
+    const suggestions = this.props.suggestions.map((product) => new ProductModel(product));
+    const { reviews } = this.props;
 
-        return (
-            <div className='single-product-cont'>
-                <Row>
-                    <Col sm={24} lg={14}>
-                        <div className='product-image'>
-                            <img src={image} alt='' />
-                        </div>
-                    </Col>
-                    <Col sm={24} lg={10} className='product-side'>
-                        <div className='product-details'>
-                            <div className='name-cont'>
-                                <h4 className='name'>{name}</h4>
-                                <h4 className='price'>{displayPrice}</h4>
-                            </div>
+    const paginatedReviews = paginate(reviews, currentPage, pageSize);
 
-                            <div
-                                className='desc'
-                                dangerouslySetInnerHTML={{
-                                    __html: description,
-                                }}
-                            ></div>
-                            <div className='rating-cont'>
-                                <StarRatingComponent
-                                    name='productStarRating'
-                                    value={average_ratings}
-                                    editing={false}
-                                />
-                            </div>
-                        </div>
-                        <div className='product-order'>
-                            <div className='quantity-cont'>
-                                {type.id === 1 ? (
-                                    <div className='quantity'>
-                                        <h5 className='title'>Quantity</h5>
-                                        <div className='counter-cont'>
-                                            <MinusOutlined
-                                                className='minus-btn'
-                                                onClick={this.onMinus}
-                                            />
-                                            <span className='order-quantity'>
-                                                {quantity}
-                                            </span>
-                                            <PlusOutlined
-                                                className='plus-btn'
-                                                onClick={this.onAdd}
-                                            />
-                                        </div>
-                                        <span>pc/s</span>
-                                    </div>
-                                ) : (
-                                    <div className='input-number'>
-                                        <Select
-                                            defaultValue={
-                                                defaultRefillableValue
-                                            }
-                                            size='large'
-                                            onChange={this.onQuantityChange}
-                                        >
-                                            {refillableOptions.map((option) => (
-                                                <Option
-                                                    key={option.value}
-                                                    value={option.value}
-                                                >
-                                                    {option.text}
-                                                </Option>
-                                            ))}
-                                        </Select>
-                                    </div>
-                                )}
+    return (
+      <div className='single-product-cont'>
+        <Row>
+          <Col sm={24} lg={14}>
+            <div className='product-image'>
+              <img src={image} alt='' />
+            </div>
+          </Col>
+          <Col sm={24} lg={10} className='product-side'>
+            <div className='product-details'>
+              <div className='name-cont'>
+                <h4 className='name'>{name}</h4>
+                <h4 className='price'>{displayPrice}</h4>
+              </div>
 
-                                <div className='subtotal-cont-single'>
-                                    <p>
-                                        Subtotal: P
-                                        <span>
-                                            {parseFloat(total).toFixed(
-                                                amountPrecision
-                                            )}
-                                        </span>
-                                    </p>
-                                </div>
-                            </div>
-                            <div className='order-button-cont'>
-                                {/* <div className='buy-now-btn'>
+              <div
+                className='desc'
+                dangerouslySetInnerHTML={{
+                  __html: description,
+                }}
+              ></div>
+              <div className='rating-cont'>
+                <StarRatingComponent name='productStarRating' value={average_ratings} editing={false} />
+              </div>
+            </div>
+            <div className='product-order'>
+              <div className='quantity-cont'>
+                {type.id === 1 ? (
+                  <div className='quantity'>
+                    <h5 className='title'>Quantity</h5>
+                    <div className='counter-cont'>
+                      <MinusOutlined className='minus-btn' onClick={this.onMinus} />
+                      <span className='order-quantity'>{quantity}</span>
+                      <PlusOutlined className='plus-btn' onClick={this.onAdd} />
+                    </div>
+                    <span>pc/s</span>
+                  </div>
+                ) : (
+                  <div className='input-number'>
+                    <Select defaultValue={defaultRefillableValue} size='large' onChange={this.onQuantityChange}>
+                      {refillableOptions.map((option) => (
+                        <Option key={option.value} value={option.value}>
+                          {option.text}
+                        </Option>
+                      ))}
+                    </Select>
+                  </div>
+                )}
+
+                <div className='subtotal-cont-single'>
+                  <p>
+                    Subtotal: P<span>{parseFloat(total).toFixed(amountPrecision)}</span>
+                  </p>
+                </div>
+              </div>
+              <div className='order-button-cont'>
+                {/* <div className='buy-now-btn'>
                                     <BuyNow
                                         product_id={id}
                                         type={type}
@@ -195,92 +159,66 @@ class SingleProductTPL extends Component {
                                         </Button>
                                     </BuyNow>
                                 </div> */}
-                                <AddCart
-                                    id={id}
-                                    type={type}
-                                    qty={quantity}
-                                    className='add-cart-btn'
-                                >
-                                    <Button type='primary' id={id} size='large'>
-                                        Add to Cart
-                                    </Button>
-                                </AddCart>
-                            </div>
-                        </div>
-                        <div className='more-products'>
-                            <p>More from {brand?.name}</p>
-                            <div className='more-product-cont'>
-                                {suggestions.map((product, i) => (
-                                    <Product
-                                        key={i}
-                                        products={product}
-                                    ></Product>
-                                ))}
-                            </div>
-                        </div>
-                        <div className='reviews-cont'>
-                            {!this.state.review ? (
-                                <div className='main-review'>
-                                    <div className='review-title'>
-                                        <h4 className='title'>Reviews</h4>
-                                        <p
-                                            className='write-btn'
-                                            onClick={this.showReview}
-                                        >
-                                            Write a Review
-                                        </p>
-                                    </div>
-                                    {reviews.length === 0 ? (
-                                        ''
-                                    ) : (
-                                        <div className='review-lists'>
-                                            {reviews.map((review, i) => (
-                                                <div key={i} className='review'>
-                                                    <h4 className='name'>
-                                                        {review.name}
-                                                    </h4>
-                                                    <StarRatingComponent
-                                                        name={i + review.name}
-                                                        value={review.ratings}
-                                                        editing={false}
-                                                    />
-                                                    <p className='para'>
-                                                        {review.message}
-                                                    </p>
-                                                </div>
-                                            ))}
-                                        </div>
-                                    )}
-                                </div>
-                            ) : (
-                                <div className='write-review'>
-                                    <div className='review-title'>
-                                        <h4 className='title'>
-                                            Rate this Product
-                                        </h4>
-                                        <p
-                                            className='write-btn'
-                                            onClick={this.showReview}
-                                        >
-                                            Cancel
-                                        </p>
-                                    </div>
-                                    <div className='review-form'>
-                                        <ReviewForm
-                                            closeForm={() =>
-                                                this.setState({ review: false })
-                                            }
-                                            product={this.props.product}
-                                        ></ReviewForm>
-                                    </div>
-                                </div>
-                            )}
-                        </div>
-                    </Col>
-                </Row>
+                <AddCart id={id} type={type} qty={quantity} className='add-cart-btn'>
+                  <Button type='primary' id={id} size='large'>
+                    Add to Cart
+                  </Button>
+                </AddCart>
+              </div>
             </div>
-        );
-    }
+            <div className='more-products'>
+              <p>More from {brand?.name}</p>
+              <div className='more-product-cont'>
+                {suggestions.map((product, i) => (
+                  <Product key={i} products={product}></Product>
+                ))}
+              </div>
+            </div>
+            <div className='reviews-cont'>
+              {!this.state.review ? (
+                <div className='main-review'>
+                  <div className='review-title'>
+                    <h4 className='title'>Reviews</h4>
+                    <p className='write-btn' onClick={this.showReview}>
+                      Write a Review
+                    </p>
+                  </div>
+                  {reviews.length === 0 ? (
+                    ''
+                  ) : (
+                    <>
+                      <div className='review-lists'>
+                        {paginatedReviews.map((review, i) => (
+                          <div key={i} className='review'>
+                            <h4 className='name'>{review.name}</h4>
+                            <StarRatingComponent name={i + review.name} value={review.ratings} editing={false} />
+                            <p className='para'>{review.message}</p>
+                          </div>
+                        ))}
+                      </div>
+                      <PageSelect itemsCount={reviews.length} pageSize={pageSize} onPageChange={this.handlePageChange} currentPage={currentPage} />
+                    </>
+                  )}
+                </div>
+              ) : (
+                <div className='write-review'>
+                  <div className='review-title'>
+                    <h4 className='title'>Rate this Product</h4>
+                    <p className='write-btn' onClick={this.showReview}>
+                      Cancel
+                    </p>
+                  </div>
+                  <div className='review-form'>
+                    <ReviewForm closeForm={() => this.setState({ review: false })} product={this.props.product}></ReviewForm>
+                  </div>
+                </div>
+              )}
+            </div>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default SingleProductTPL;
