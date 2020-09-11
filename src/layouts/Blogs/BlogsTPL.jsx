f5895cca21d29dfa5e28e97e18960c5bcd9ff47e
Darren Hizon <darren.hizon@nuworks.ph>
2020-09-11 19:50:39 +0800

    Done with responsive

--- src/layouts/Blogs/BlogsTPL.jsx
+++ src/layouts/Blogs/BlogsTPL.jsx
@@ -4,79 +4,69 @@ import Link from 'next/link';
 import Blog from '../../components/blog/Blog';
 import ModalVideo from 'react-modal-video';
 import SocialService from '~/services/SocialService';
+import { Row, Col } from 'antd';
 
 class BlogsTPL extends Component {
-    state = {
-        video: '',
-        open: false,
-        blogs: [],
-    };
+  state = {
+    video: '',
+    open: false,
+    blogs: [],
+  };
 
-    componentDidMount() {
-        (async () => {
-            const videos = await SocialService.videos();
-            const links = await SocialService.links();
-            const articles = await SocialService.articles();
-            console.log(articles);
-            this.setState({ blogs: [...videos, ...links, ...articles] });
-        })();
-    }
+  componentDidMount() {
+    (async () => {
+      const videos = await SocialService.videos();
+      const links = await SocialService.links();
+      const articles = await SocialService.articles();
+      console.log(articles);
+      this.setState({ blogs: [...videos, ...links, ...articles] });
+    })();
+  }
 
-    onOpen = (video) => {
-        this.setState({ video }, this.openModal);
-    };
+  onOpen = (video) => {
+    this.setState({ video }, this.openModal);
+  };
 
-    openModal = () => {
-        this.setState({ open: true });
-    };
+  openModal = () => {
+    this.setState({ open: true });
+  };
 
-    closeModal = () => {
-        this.setState({ open: false });
-    };
+  closeModal = () => {
+    this.setState({ open: false });
+  };
 
-    render() {
-        const { open, video } = this.state;
+  render() {
+    const { open, video } = this.state;
 
-        let { blogs } = this.state;
+    let { blogs } = this.state;
 
-        return (
-            <>
-                <ModalVideo
-                    channel='youtube'
-                    isOpen={open}
-                    videoId={video}
-                    onClose={this.closeModal}
-                />
-                <div className='blog-banner'></div>
-                <Container>
-                    <div className='blogs-cont'>
-                        <h1 className='title'>BLOGS</h1>
-                        <div className='blog-list'>
-                            {blogs.map((blog, i) =>
-                                blog.type === 'video' ? (
-                                    <Blog
-                                        key={i}
-                                        type={blog.type}
-                                        image={blog.image}
-                                        title={blog.title}
-                                        onClick={() => this.onOpen(blog.video)}
-                                    ></Blog>
-                                ) : (
-                                    <Blog
-                                        key={i}
-                                        type={blog.type}
-                                        image={blog.image}
-                                        title={blog.title}
-                                        link={blog.link}
-                                    ></Blog>
-                                )
-                            )}
-                        </div>
-                    </div>
-                </Container>
-            </>
-        );
-    }
+    return (
+      <>
+        <ModalVideo channel='youtube' isOpen={open} videoId={video} onClose={this.closeModal} />
+        <div className='blog-banner'></div>
+        <Container>
+          <div className='blogs-cont'>
+            <h1 className='title'>BLOGS</h1>
+            <div className='blog-list'>
+              <Row gutter={16}>
+                {blogs.map((blog, i) =>
+                  blog.type === 'video' ? (
+                    <Col xs={24} md={8}>
+                      <Blog key={i} type={blog.type} image={blog.image} title={blog.title} onClick={() => this.onOpen(blog.video)}></Blog>
+                    </Col>
+                  ) : (
+                    <Col xs={24} md={8}>
+                      <Blog key={i} type={blog.type} image={blog.image} title={blog.title} link={blog.link}></Blog>
+                    </Col>
+                  )
+                )}
+              </Row>
+            </div>
+          </div>
+        </Container>
+      </>
+    );
+  }
 }
 
 export default BlogsTPL;
--- src/layouts/Blogs/SingleTPL.jsx
+++ src/layouts/Blogs/SingleTPL.jsx
@@ -4,66 +4,64 @@ import Blog from '../../components/blog/Blog';
 import { useEffect, useState } from 'react';
 import SocialServices from '../../services/SocialService';
 import { useRouter } from 'next/router';
+import { Row, Col } from 'antd';
 
 const BlogArticlePage = () => {
-    const [data, setData] = useState({});
-    const [articles, setArticles] = useState([]);
+  const [data, setData] = useState({});
+  const [articles, setArticles] = useState([]);
 
-    const router = useRouter();
+  const router = useRouter();
 
-    useEffect(() => {
-        (async () => {
-            try {
-                const id = router.query?.slug;
+  useEffect(() => {
+    (async () => {
+      try {
+        const id = router.query?.slug;
 
-                const articleData = await SocialServices.articleById(id);
-                setData(articleData);
-                const relatedArticles = await SocialServices.getRelated(id);
-                setArticles(relatedArticles);
-            } catch (e) {
-                console.log(e);
-            }
-        })();
-    }, [router.query.slug]);
+        const articleData = await SocialServices.articleById(id);
+        setData(articleData);
+        const relatedArticles = await SocialServices.getRelated(id);
+        setArticles(relatedArticles);
+      } catch (e) {
+        console.log(e);
+      }
+    })();
+  }, [router.query.slug]);
 
-    return (
-        <>
-            <Container>
-                <div className='single-blogs-cont'>
-                    <h1 className='title'>{data.title}</h1>
-                    <p className='date'>{data.publishedAt}</p>
+  return (
+    <>
+      <Container>
+        <div className='single-blogs-cont'>
+          <h1 className='title'>{data.title}</h1>
+          <p className='date'>{data.publishedAt}</p>
 
-                    <div className='image-cont'>
-                        <img src={data.image} alt='' />
-                        {/* <p className='desc'>Test</p> */}
-                    </div>
+          <div className='image-cont'>
+            <img src={data.image} alt='' />
+            {/* <p className='desc'>Test</p> */}
+          </div>
 
-                    <div
-                        className='body'
-                        dangerouslySetInnerHTML={{
-                            __html: data.content,
-                        }}
-                    ></div>
+          <div
+            className='body'
+            dangerouslySetInnerHTML={{
+              __html: data.content,
+            }}
+          ></div>
 
-                    <div className='more-blogs'>
-                        <h1 className='title'>You Might Also Like</h1>
-                        <div className='more-blog-list'>
-                            {(articles || []).map((article, i) => (
-                                <Blog
-                                    key={i}
-                                    id={article.id}
-                                    type={article.type}
-                                    image={article.image}
-                                    title={article.title}
-                                    link={article.link}
-                                ></Blog>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-            </Container>
-        </>
-    );
+          <div className='more-blogs'>
+            <h1 className='title'>You Might Also Like</h1>
+            <div className='more-blog-list'>
+              <Row gutter={16}>
+                {(articles || []).map((article, i) => (
+                  <Col xs={24} md={8}>
+                    <Blog key={i} id={article.id} type={article.type} image={article.image} title={article.title} link={article.link}></Blog>
+                  </Col>
+                ))}
+              </Row>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default BlogArticlePage;
